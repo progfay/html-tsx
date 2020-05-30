be02eb1365045d2603d29aac66e65be68c376f17
progfay <shun1856shun@gmail.com>
2020-05-30 18:52:27 +0900

    feat: type check for children

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,82 @@
 declare global {
   namespace JSX {
-    type PropertyType = boolean | number | string
+    type EmptyElementTagName = (keyof HTMLElementTagNameMap) & (
+      | 'area'
+      | 'base'
+      | 'br'
+      | 'col'
+      | 'embed'
+      | 'hr'
+      | 'img'
+      | 'input'
+      | 'keygen'
+      | 'link'
+      | 'meta'
+      | 'param'
+      | 'source'
+      | 'track'
+      | 'wbr'
+    )
 
-    type AttributesType = {
-      [key: string]: PropertyType
+    interface ElementChildrenAttribute {
+      children: string | string[]
+    }
+
+    type Properties<T extends keyof HTMLElementTagNameMap> =
+    {
+      [K in keyof Omit<HTMLElementTagNameMap[T], keyof JSX.ElementChildrenAttribute>]?:
+        HTMLElementTagNameMap[T][K] extends (string | boolean | number)
+          ? HTMLElementTagNameMap[T][K]
+          : never
+    } & {
+      [K in keyof JSX.ElementChildrenAttribute]?:
+        T extends JSX.EmptyElementTagName ? never : JSX.ElementChildrenAttribute[K]
     }
 
     type IntrinsicElements = {
-      [tag in keyof ElementTagNameMap]: AttributesType | {}
+      [T in keyof HTMLElementTagNameMap]?: Properties<T>
     }
   }
 }
 
-const renderAttributes = (attr: JSX.AttributesType): string => (
-  Object
+// Reference: https://developer.mozilla.org/en-US/docs/Glossary/Empty_element
+const EMPTY_ELEMENT_LIST = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+] as const
+
+function renderAttributes<T extends keyof JSX.IntrinsicElements> (attr: JSX.Properties<T>): string {
+  return Object
     .entries(attr)
-    .map(([key, value]) => typeof value === 'boolean' ? key : `${key}="${value.toString().replace(/&/g, '&amp;').replace(/"/g, '&quot;')}"`)
+    .map(([key, value]) => (
+      typeof value === 'boolean'
+        ? key
+        : `${key}="${value?.toString().replace(/&/g, '&amp;').replace(/"/g, '&quot;') ?? ''}"`
+    ))
     .join(' ')
-)
+}
 
-const renderInnerHTMLs = (innerHTMLs: (number | string | boolean)[]): string => (
-  innerHTMLs.map(
-    (innerHTML = '') => (
-      Array.isArray(innerHTML)
-        ? innerHTML.map(html => html.toString()).join('')
-        : innerHTML.toString()
-    )
-  ).join('')
-)
-
-export const pragma = (tagName: string, attributes: JSX.AttributesType, ...innerHTMLs: (number | string | boolean)[]): string =>
-  `<${tagName}${attributes ? ` ${renderAttributes(attributes)}` : ''}>${renderInnerHTMLs(innerHTMLs)}</${tagName}>`
+export function pragma<T extends keyof JSX.IntrinsicElements> (
+  tagName: T,
+  properties?: JSX.Properties<T>,
+  ...children: JSX.ElementChildrenAttribute['children'][]
+): string {
+  return (
+    tagName in EMPTY_ELEMENT_LIST
+      ? `<${tagName}${properties ? ` ${renderAttributes(properties)}` : ''}>`
+      : `<${tagName}${properties ? ` ${renderAttributes(properties)}` : ''}>${children.join('')}</${tagName}>`
+  )
+}
